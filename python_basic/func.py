1f736d867d9f4d84e707b4523043f3f011d4f575
Vithoba Gorakh Bunge <mr.vithubunge01@gmail.com>
2026-09-14 15:33:11 +0530

    Initial Commit

---
+++ python_basic/func.py
@@ -0,0 +1,10 @@
+
+def trees(m,a,e):
+    result = m+a+e
+
+    print(f"your morning spend is {m}")
+    print(f"your afternoon spend is {a}")
+    print(f"your evening spend is {e}")
+    print(f"your total spend is {result}")
+
+    return result 
